37e8559b85f56f3ce81ef254535e53a48e5dfc56
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-01-31 18:47:08 +0000

    added a way to select which folder to use when saving files

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Configuration;
 
 namespace ExpenseTracker.Controllers
 {
     public class SettingsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public SettingsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        //ToDo: change from hardcoded to dynamic when possible
+        public async Task<IActionResult> Index(int id = 1)
+        {
+            if (id == 0)
+                return View(new Models.Settings());
+            else
+                return View(_context.Settings.Find(id));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveSettings(Models.Settings settings)
+        {
+            if (ModelState.IsValid)
+            {
+                if (settings.SettingsId == 0)
+                    _context.Add(settings);
+                else                
+                    _context.Update(settings);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));                
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
